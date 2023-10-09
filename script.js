1a151507b6a2316819a20eab3cbacdd9509c669d
Zachary Elbring <elbringzach@gmail.com>
2023-10-09 16:11:37 -0500

    Added javascript variables and assigned values.

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // Assignment code here
 
+var length = 8;
+var choices = [];
+var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var numeric = ['0','1','2','3','4','5','6','7','8','9'];
+var specialCharacters = [' ','!','"','#','$','%','&','*','+','-','.','<','>','=','?','@'];
+function generatePassword() {
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
