ceffcabe2aa402f9c22259c768e582d1925c5265
Zachary Elbring <elbringzach@gmail.com>
2023-10-12 16:38:48 -0500

    Added more comments to javascript code and finished readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # random-password-generator
+
+## Description
+This random password generator was created to randomly generate secure passwords based on the choices of characters chosen by the user. This assignment helped me feel more comfortable using Javascript and Javascript functions.
+
+## Credits
+Starter code: https://github.com/coding-boot-camp/friendly-parakeet
+MDN Web Docs on Javascript: https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+## Screenshot
+![Screenshot of webpage.](elbringz.github.io_random-password-generator_.png)
+
+## Link to deployed application
+https://elbringz.github.io/random-password-generator/
--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // Assignment code here
-
+// Sets variables to be called inside of functions
 var length = 8;
 var choices = [];
 var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var numeric = ['0','1','2','3','4','5','6','7','8','9'];
 var specialCharacters = [' ','!','"','#','$','%','&','*','+','-','.','<','>','=','?','@'];
+
+// This function generates the password based on the choices made in the givePrompts function
 function generatePassword() {
 var password = '';
+
+// This for loop sets a variable for generating random characters based on the length chosen by the user
 for(var x = 0; x < length; x++){
   var randomChar = Math.floor(Math.random() * choices.length);
   password = password + choices[randomChar];
@@ -15,14 +19,20 @@ for(var x = 0; x < length; x++){
 return password;
 }
 
+// This function gives prompts for the password length, uppercase, lowercase, numbers, and special characters
 function givePrompts(){
   choices = [];
+
+  // parseInt parses the value as a string and returns integer
   length = parseInt(prompt("How many characters should your password contain? (8-128 characters)"));
+
+  // This conditional statement forces the password length to be between 8 and 128 characters
   if(length < 8 || length > 128){
     alert('Password length must be a number 8-128. Please try again');
     return false;
   }
 
+  // These conditional statements are used to ask the user which types of characters they would like to be included in their password
   if(confirm('Would you like uppercase letters to be included in your password?')) {
     choices = choices.concat(uppercase);
   }
